5c6695df3a0cd68a2129b04725c0d137691b6efa
juanpo12 <juanjoc2000@hotmail.com>
2024-03-18 09:22:14 -0300

    feat[component] Created Input

---
+++ renderer/components/Input.tsx
@@ -0,0 +1,17 @@
+interface InputProps {
+    type: string
+    label?: string
+    setValue: React.Dispatch<React.SetStateAction<any>>
+    name: string
+}
+const Input = ({type, label, setValue, name} : InputProps) => {
+
+    return (
+        <div>
+            {label && <label className="">{label}</label>}
+            <input className="bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 m-2 " type={type} name={name} onChange={(e) => setValue(e.target.value)}/>
+        </div>
+    )
+}
+
+export default Input
