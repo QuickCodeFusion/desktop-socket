c2f9978d4b42e53f1038563b7de6de54732c9242
juanpo12 <juanjoc2000@hotmail.com>
2024-03-18 10:27:42 -0300

    change color input

--- renderer/components/ui/Input.tsx
+++ renderer/components/ui/Input.tsx
@@ -2,14 +2,15 @@ interface InputProps {
     type: string
     label?: string
     setValue: React.Dispatch<React.SetStateAction<any>>
-    name: string
+    name: string,
+    value: string
 }
 const Input = ({type, label, setValue, name} : InputProps) => {
 
     return (
         <div className="flex items-center justify-self-end m-2">
             {label && <label className="">{label}</label>}
-            <input className="bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 m-2 " type={type} name={name} onChange={(e) => setValue(e.target.value)}/>
+            <input className="bg-gray-100 border text-gray-950 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 m-2 " type={type} name={name} onChange={setValue}/>
         </div>
     )
 }
