9cd9f35a0f17612b9ea7750f7036576ebf0448e9
Uriel Campos <urielcampos448@gmail.com>
2024-03-20 18:07:27 -0300

    feat[ui/Title] updated className

--- renderer/components/ui/Title.tsx
+++ renderer/components/ui/Title.tsx
@@ -2,9 +2,9 @@ interface TitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
     
 }
 
-export const Title: React.FC<TitleProps> = ({children}) => {
+export const Title: React.FC<TitleProps> = ({children, ...props}) => {
     return (
-        <h1 className="text-lg first-letter:uppercase font-semibold">
+        <h1 {...props} className={`text-lg first-letter:uppercase font-semibold ${props.className}`}>
             {children}
         </h1>
     )
