8cdbf83296d4b82fc7d86bd88bb81c5d50fb09b1
Uriel Campos <urielcampos448@gmail.com>
2024-03-25 10:59:28 -0300

    rendering SceneChange on home page

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,13 +1,18 @@
+import { SceneChange } from '@/components/SceneChange'
 import Head from 'next/head'
 import Link from 'next/link'
+import { useState } from 'react'
 export default function HomePage() {
+  const [scene, setScene] = useState('')
   return (
     <>
       <Head>
         <title>Home - Nextron (with-tailwindcss)</title>
       </Head>
       
-
+      <main className="bg-neutral-800 text-white h-screen">
+        <SceneChange scene={scene} setScene={setScene} />
+      </main>
     </>
   )
 }
